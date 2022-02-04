5037d854fdcd2d3cfd81dad38bab9b569e9a1361
Aftab Mohammed (el21am) <94575661+aftaaab@users.noreply.github.com>
2022-02-04 11:26:09 +0000

    Update Code.m

--- FTPA in 2 User NOMA/Code.m
+++ FTPA in 2 User NOMA/Code.m
@@ -3,7 +3,8 @@ BW = 10^6; % Bandwidth (Hz)
 P_dBm = 0:1:40;             % Transmission power (dBm)
 P = (10^-3)*10.^(P_dBm/10);   % Transmission power (linear)
 
-%Fixed Power Allocation
+%Fractional Transmit Power Allocation (FTPA)
+
 % Weak User
 d1 = 1000;
 a1 = 0.75;
