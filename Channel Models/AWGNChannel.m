e43c218a497eb0b21b922e259c522ce023f910d2
Lewis <el19lsn@leeds.ac.uk>
2022-04-20 13:01:15 +0100

    Support multiple channels

--- Channel Models/AWGNChannel.m
+++ Channel Models/AWGNChannel.m
@@ -1,13 +1,14 @@
-function noise = AWGNChannel(s, EbNo_dB, k)
-%   Returns AWGN noise samples corresponding to Eb/No
-%   Assumes 1 sample per symbol
-%   s - input signal
-%   EbNo - SNR per bit in decibels
-%   k - bits per symbol
+function out = AWGNChannel(s, EbNo_dB, k)
+    %   Returns AWGN noise samples corresponding to Eb/No
+    %   Assumes 1 sample per symbol
+    %   s - input signal (each column represents a channel)
+    %   EbNo - SNR per bit in decibels
+    %   k - bits per symbol
 
     EbNo = 10^(EbNo_dB/10); % Convert EbNo(dB) to linear EbNo
     Eb = (sum(abs(s).^2)/length(s))/k; % Calculate energy per bit
     sigma = sqrt((Eb/EbNo)/2); % Calculate No and normalise; convert to amplitude
-    noise = sigma*(randn(1,length(s))+1i*randn(1,length(s))); % Calculate noise samples
-
+    n = sigma.*(randn(length(s), width(s))+1i*randn(length(s), width(s))); % Calculate noise samples
+    
+    out = s + n;
 end
